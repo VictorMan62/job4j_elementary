0505f9cba22ddec1b122ec93ff5975ecd3eb7985
Victor <manvic@ya.ru>
2020-07-25 21:03:36 +0300

    9.1. String [#332550]

---
+++ src/ru/job4j/Greeting.java
@@ -0,0 +1,13 @@
+package ru.job4j;
+
+public class Greeting {
+    public static void main(String[] args) {
+        String space = " ";
+        String idea = "I like Java!";
+        System.out.println(idea);
+        String newbie = "But I am a newbie.";
+        System.out.println(idea+space+newbie);
+        int year = 2020;
+        System.out.println(idea+space+newbie+space+year+".");
+    }
+}
