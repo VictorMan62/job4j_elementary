0437fb8c0e9658c6291dfee1eccaa376959e045d
Victor <manvic@ya.ru>
2020-07-25 23:10:53 +0300

    0. Что такое тестирование [#332578]

--- src/ru/job4j/Converter.java
+++ src/ru/job4j/Converter.java
@@ -2,20 +2,25 @@ package ru.job4j;
 
 public class Converter {
 
-    public static int rubleToEuro(int value) {
-        int rsl = value / 70;
-        return rsl;
+    public static double rubleToEuro(double value) {
+        return value / 70;
     }
 
     public static double rubleToDollar(double value) {
-        double rsl = value / 60;
-        return rsl;
+        return value / 60;
     }
 
     public static void main(String[] args) {
-        int euro = Converter.rubleToEuro(140);
+        double euro = Converter.rubleToEuro(140);
         double dollars = Converter.rubleToDollar(140);
-        System.out.println("140 rubles are " + euro + " euro.");
-        System.out.println("140 rubles are " + dollars + " dollars.");
+        double in = 140;
+        double expectedeuro = 2;
+        double expecteddollar = 2.33;
+        double oute = rubleToEuro(in);
+        double outd = rubleToDollar(in);
+        boolean passede = expectedeuro == oute;
+        boolean passedd = expecteddollar == outd;
+        System.out.println("140 rubles are 2 euro. Test result : " + passede);
+        System.out.println("140 rubles are 2 dollars. Test result : " + passedd);
     }
 }
--- src/ru/job4j/StudentInfo.java
+++
@@ -1,8 +0,0 @@
-package ru.job4j;
-
-public class StudentInfo {
-    public static void main(String[] args) {
-        System.out.println("Victor Manakin");
-        System.out.println("07.08.1982");
-    }
-}
