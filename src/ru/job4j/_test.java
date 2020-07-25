e61138476d79eaf32ddb830ebbde857e199cd5f5
Victor <manvic@ya.ru>
2020-07-25 23:17:41 +0300

    0. Что такое тестирование [#332578]

---
+++ src/ru/job4j/_test.java
@@ -0,0 +1,11 @@
+package ru.job4j;
+
+public class _test {
+    public static void main(String[] args) {
+        float floatNum = 2.6f;
+        int integerNum = (int) floatNum;
+        System.out.println(integerNum);
+    }
+}
+
+
