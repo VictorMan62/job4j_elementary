592ae6c0b0d55d24e6d7d28632cf2b3e3d30fcde
Victor <manvic@ya.ru>
2020-07-26 09:26:36 +0300

    9.1. String [#332550]

--- src/ru/job4j/Greeting.java
+++ src/ru/job4j/Greeting.java
@@ -2,7 +2,6 @@ package ru.job4j;
 
 public class Greeting {
     public static void main(String[] args) {
-        String space = " ";
         String idea = "I like Java!";
         System.out.println(idea);
         String newbie = idea + " But I am a newbie. ";
