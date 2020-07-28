4a6d6cd2d63cffca86aea5f582d24db40f7951bf
Victor <manvic@ya.ru>
2020-07-28 12:30:35 +0300

    Операции сравнения [#332500]

---
+++ src/ru/job4j/Condition.java
@@ -0,0 +1,16 @@
+package ru.job4j;
+
+    public class Condition {
+        public static class Liken {
+            public static void main(String[] args) {
+                int first = 10;
+                int second = 9;
+                boolean result = first > second;
+                        System.out.println(result);
+                result = first < second;
+                        System.out.println(result);
+                result = first == second;
+                        System.out.println(result);
+            }
+        }
+}
