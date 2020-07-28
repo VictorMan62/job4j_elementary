e25e0d35eac79b3e209390692ea8474bce1de635
Victor <manvic@ya.ru>
2020-07-28 12:16:42 +0300

    1.1. Входные данные в тесте [#332584] final 2

--- src/ru/job4j/Calculate.java
+++ src/ru/job4j/Calculate.java
@@ -1,24 +1,23 @@
 package ru.job4j;
 
+    public class Calculate {
 
-public class Calculate {
+            public static int calc(int a, int b, int c, int x) {
+                return a * x * x + b * x + c;
+            }
 
-        public static int calc(int a, int b, int c, int x) {
-            return a * x * x + b * x + c;
-        }
-
-        public static void main(String[] args) {
-            int a = 10;
-            int b = 0;
-            int c = 0;
-            int x = 1;
-            int rsl1 = Calculate.calc(a, b, c, x);
-            int rsl2 = Calculate.calc(a, b, c, x);
-            int rsl3 = Calculate.calc(a, b, c, x);
-            int rsl4 = Calculate.calc(a, b, c, x);
-            System.out.println(rsl1);
-            System.out.println(rsl2);
-            System.out.println(rsl3);
-            System.out.println(rsl4);
-        }
+            public static void main(String[] args) {
+                int a = 10;
+                int b = 0;
+                int c = 0;
+                int x = 1;
+                int rsl1 = Calculate.calc(a, b, c, x);
+                int rsl2 = Calculate.calc(a, b, c, x);
+                int rsl3 = Calculate.calc(a, b, c, x);
+                int rsl4 = Calculate.calc(a, b, c, x);
+                System.out.println(rsl1);
+                System.out.println(rsl2);
+                System.out.println(rsl3);
+                System.out.println(rsl4);
+            }
 }
