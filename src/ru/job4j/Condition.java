97f69367e6052b730a7b1fd1deff0e457f5e31af
Victor <manvic@ya.ru>
2020-07-28 13:14:23 +0300

    4.1. Операторы ветвлений [#332507]

--- src/ru/job4j/Condition.java
+++ src/ru/job4j/Condition.java
@@ -1,16 +1,30 @@
 package ru.job4j;
 
-    public class Condition {
-        public static class Liken {
-            public static void main(String[] args) {
-                int first = 10;
-                int second = 9;
-                boolean result = first > second;
-                        System.out.println(result);
-                result = first < second;
-                        System.out.println(result);
-                result = first == second;
-                        System.out.println(result);
-            }
+/*public class Condition {
+
+        public static void main(String[] args) {
+            int first = 10;
+            int second = 9;
+            boolean result = first > second;
+                    System.out.println(result);
+            result = first < second;
+                    System.out.println(result);
+            result = first == second;
+                    System.out.println(result);
+        }*/
+    class AlertDivByZero {
+    public static void main(String[] args) {
+        possibleDiv(4);
+        possibleDiv(-2);
+        possibleDiv(-3);
+        possibleDiv(5);
+        possibleDiv(-21);
+    }
+
+    public static void possibleDiv(int number) {
+        if (number < 0) {
+            System.out.println(number + " is negative number");
         }
+    }
+
 }
